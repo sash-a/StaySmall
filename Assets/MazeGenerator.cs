e70d420b08904b277ea296fefea1635ebe5a34e6
Sasha Abramowitz <reallysasha@gmail.com>
2018-03-31 01:22:47 +0200

    Adding missing files from last commit

---
+++ Assets/MazeGenerator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public GameObject block;
+
+    public int width;
+    public int height;
+
+    private Cell[,] maze;
+    private Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+    // Use this for initialization
+    void Start()
+    {
+        maze = new Cell[width, height];
+        genMaze(0, 0);
+        displayMaze();
+    }
+
+    private GameObject placeBlock(Vector3 pos, bool vert)
+    {
+        GameObject piece = Instantiate(block, pos, Quaternion.identity);
+
+        if (!vert) return piece;
+        var trans = piece.GetComponent<Transform>();
+        trans.Rotate(0, 0, 90);
+        trans.position -= new Vector3(trans.localScale.x, trans.localScale.x) * 2;
+
+        return piece;
+    }
+
+
+    void genMaze(int cx, int cy)
+    {
+        var randDirections = Cell.directions.OrderBy(x => rnd.Next()).ToList();
+
+        foreach (var dir in randDirections)
+        {
+            int nx = cx + Cell.dx[dir], ny = cy + Cell.dy[dir];
+
+            if (0 <= ny && ny < height && 0 <= nx && nx < width && maze[nx, ny] == null)
+            {
+                if (maze[cx, cy] == null) maze[cx, cy] = new Cell();
+                if (maze[nx, ny] == null) maze[nx, ny] = new Cell();
+
+                maze[cx, cy].addDir(dir);
+                maze[nx, ny].addDir(Cell.opposite[dir]);
+                genMaze(nx, ny);
+            }
+        }
+    }
+
+    void displayMaze()
+    {
+        float alleyWidth = block.GetComponent<Transform>().localScale.x * 4;
+        //Vector3 initialPos = new Vector3(1, 1, 1);
+        //float corridorWidth = 2;
+
+        for (int w = 0; w < width; w++)
+        {
+            for (int h = 0; h < height; h++)
+            {
+                Cell cell = maze[w, h];
+                var pos = new Vector3(w, h, 0);
+                if (!cell.n)
+                {
+                    var g = placeBlock(pos * alleyWidth, false);
+                    g.GetComponent<SpriteRenderer>().color = Color.blue;
+                }
+
+                if (!cell.s)
+                {
+                    var g = placeBlock(pos * alleyWidth + new Vector3(0, -alleyWidth, 0), false);
+                    g.GetComponent<SpriteRenderer>().color = Color.red;
+                }
+
+                if (!cell.w)
+                {
+                    var g = placeBlock(pos * alleyWidth, true);
+                    g.GetComponent<SpriteRenderer>().color = Color.green;
+                }
+
+                if (!cell.e)
+                {
+                    var g = placeBlock(pos * alleyWidth + new Vector3(alleyWidth, 0, 0), true);
+                    g.GetComponent<SpriteRenderer>().color = Color.white;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Just a container for north south east and west directions
+ */
+
+using System.Collections.Generic;
+
+public class Cell
+{
+    public bool n;
+    public bool s;
+    public bool w;
+    public bool e;
+
+    private static string north = "n", south = "s", west = "w", east = "e";
+    public static List<string> directions = new List<string> {north, south, west, east};
+
+    public static Dictionary<string, int> dx = new Dictionary<string, int>
+    {
+        {north, 0},
+        {south, 0},
+        {west, -1},
+        {east, 1}
+    };
+
+    public static Dictionary<string, int> dy = new Dictionary<string, int>
+    {
+        {north, 1},
+        {south, -1},
+        {west, 0},
+        {east, 0}
+    };
+
+    public static Dictionary<string, string> opposite = new Dictionary<string, string>
+    {
+        {north, south},
+        {south, north},
+        {west, east},
+        {east, west}
+    };
+
+    public Cell()
+    {
+        n = false;
+        s = false;
+        w = false;
+        e = false;
+    }
+
+    public void addDir(string dir)
+    {
+        if (dir.ToLower().Equals("n"))
+        {
+            n = true;
+        }
+        else if (dir.ToLower().Equals("s"))
+        {
+            s = true;
+        }
+        else if (dir.ToLower().Equals("w"))
+        {
+            w = true;
+        }
+        else if (dir.ToLower().Equals("e"))
+        {
+            e = true;
+        }
+    }
+}
